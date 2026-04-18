8bc1a595bb93ce5299cffe90f9921eaad9a8fa94
tae2089 <tae2089002@gmail.com>
2026-04-18 11:25:03 +0900

    feat: update Lua tree-sitter queries to include method index expressions and local function definitions

--- internal/parse/treesitter/queries/lua/tags.scm
+++ internal/parse/treesitter/queries/lua/tags.scm
@@ -1,7 +1,17 @@
 ; Functions
 (function_statement
-  name: [(function_name) (identifier)] @name.function) @definition.function
+  name: [
+    (function_name) 
+    (identifier)
+    (method_index_expression)
+  ] @name.function) @definition.function
+
+(local_function_statement
+  name: (identifier) @name.function) @definition.function
 
 ; Calls
 (function_call
-  prefix: (identifier) @name.call) @reference.call
+  prefix: [
+    (identifier)
+    (method_index_expression)
+  ] @name.call) @reference.call
